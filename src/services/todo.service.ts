421588acd5802bc58577d828216228fd353e7eee
Shane Lin <shanelin@cryptoxlab.com>
2024-11-18 15:43:02 +0800

    feat(): implement todo service

---
+++ src/services/todo.service.ts
@@ -0,0 +1,19 @@
+import {injectable, BindingScope} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {TodoRepository} from '../repositories';
+import {Todo, Item} from '../models';
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class TodoService {
+  constructor(
+    @repository(TodoRepository) public todoRepository: TodoRepository,
+  ) {}
+
+  async createTodoWithItems(todoData: Todo, itemsData: Item[]): Promise<Todo> {
+    const todo = await this.todoRepository.create(todoData);
+    for (const item of itemsData) {
+      await this.todoRepository.items(todo.id).create(item);
+    }
+    return todo;
+  }
+}

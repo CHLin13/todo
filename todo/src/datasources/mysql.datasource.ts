be87e43d4f0af8f4bbc09a2fccf5c63d2bc4e310
Shane Lin <shanelin@cryptoxlab.com>
2024-11-19 13:36:06 +0800

    refactor(): replace info with env

--- todo/src/datasources/mysql.datasource.ts
+++ todo/src/datasources/mysql.datasource.ts
@@ -1,14 +1,16 @@
 import {juggler} from '@loopback/repository';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
   name: 'mysql',
   connector: 'mysql',
   url: '',
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '123123',
-  database: 'todo',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 };
 
 export class MysqlDataSource extends juggler.DataSource {
